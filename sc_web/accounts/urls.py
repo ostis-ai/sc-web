ac6a12ac5b6a6d907be66807985d125efac04811
Nikita Grishko <grin.minsk@gmail.com>
2013-03-17 00:30:53 +0300

    Simple login/logout based on django.contrib.auth

--- sc_web/accounts/urls.py
+++ sc_web/accounts/urls.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import patterns, url
+from django.contrib.auth import views as auth_views
 
 from accounts.views import RegistrationView
 
 
 urlpatterns = patterns('',
     url(r'^registration/$', RegistrationView.as_view(), name='registration'),
+    url(r'^login/$', auth_views.login, {
+        'template_name': 'accounts/login.html',
+        'redirect_field_name': 'next_url',
+        }, name='login'),
+    url(r'^logout/$', auth_views.logout, {
+        'next_page': '/',
+        }, name='logout'),
 )
