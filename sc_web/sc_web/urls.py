3bd5a5d931055e5bcf6b883aa49de1ff40eb4581
Denis Koronchik <denis.koronchik@gmail.com>
2013-03-16 17:02:17 +0300

    fixed bug with static url rule

--- sc_web/sc_web/urls.py
+++ sc_web/sc_web/urls.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from django.conf import settings as app_settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -10,4 +11,7 @@ urlpatterns = patterns('',
     url(r'^api/', include('api.urls', namespace='api')),
 
     url(r'^admin/', include(admin.site.urls)),
+    
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': app_settings.STATIC_ROOT})
 )
