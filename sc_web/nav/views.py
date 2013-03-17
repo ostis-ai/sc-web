23b5e291d48eb600238739413b6b7103e308e10f
Nikita Grishko <grin.minsk@gmail.com>
2013-03-17 13:48:10 +0300

    Login modal dialog

--- sc_web/nav/views.py
+++ sc_web/nav/views.py
@@ -24,7 +24,9 @@ along with OSTIS. If not, see <http://www.gnu.org/licenses/>.
 import json
 import time
 
+from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView
+from django.utils.decorators import method_decorator
 
 from sctp.logic import new_sctp_client
 
@@ -40,6 +42,10 @@ class HomeView(TemplateView):
 
 class StatView(TemplateView):
     template_name = 'nav/stat.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(StatView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(StatView, self).get_context_data(**kwargs)
