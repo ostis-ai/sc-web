3b2d301e811f6ec647eb77a3d6836d47eaedcc29
Nikita Grishko <grin.minsk@gmail.com>
2013-03-26 19:14:21 +0300

    Fix prepare_js scripts

--- prepare.bat
+++ prepare.bat
@@ -1,1 +1,1 @@
-"sc-web-core/build/concatenate_js.py" "sc-web-core/js/" "sc_web/static/js/sc-web-core.js"
+"sc-web-core/build/concatenate_js.py" "sc-web-core/js/" "sc_web/sc_web/static/sc_web/js/sc-web-core.js"
