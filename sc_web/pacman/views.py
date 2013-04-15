9216893b10a1e06ce1dbc00d63aa30a857628ca1
Nikita Grishko <grin.minsk@gmail.com>
2013-04-15 11:48:00 +0300

    Admin extras for pacman (issue #61)

--- sc_web/pacman/views.py
+++ sc_web/pacman/views.py
@@ -1,11 +1,51 @@
 # -*- coding: utf-8 -*-
 
-from django.views.generic.base import TemplateView
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.views.generic.base import TemplateView, View
+from django.utils.decorators import method_decorator
+
+from pacman.tasks import PullTask, PushTask, DownloadTask
 
 __all__ = (
+    'PullTaskView',
+    'PushTaskView',
+    'DownloadTask',
+
     'PackgesView',
     'PackgeView',
 )
+
+
+class BaseTaskView(View):
+    get = View.http_method_not_allowed
+    redirect_url = reverse_lazy('admin:pacman_package_changelist')
+    task = None
+
+    @method_decorator(login_required(login_url=reverse_lazy('admin:index')))
+    def dispatch(self, request, *args, **kwargs):
+        return super(BaseTaskView, self).dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.task.delay()
+        messages.add_message(
+            request, messages.INFO, '"%s" task sended to broker.' % self.task.name
+        )
+        return HttpResponseRedirect(self.redirect_url)
+
+
+class PullTaskView(BaseTaskView):
+    task = PullTask
+
+
+class PushTaskView(BaseTaskView):
+    task = PushTask
+
+
+class DownloadTaskView(BaseTaskView):
+    task = DownloadTask
 
 
 class PackagesView(TemplateView):
