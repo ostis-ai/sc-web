95f74e998d5a4d14225771ea9929df047224b189
Denis Koronchik <denis.koronchik@gmail.com>
2014-01-24 02:20:32 +0300

    add missed file; small fixes in search panel style

---
+++ sc-web-core/js/Ui/searchpanel.js
@@ -0,0 +1,38 @@
+SCWeb.ui.SearchPanel = {
+    
+    init: function() {
+        var dfd = new jQuery.Deferred();
+
+        this.addrs = {}
+        var self = this;
+
+        $('.typeahead').typeahead({
+            minLength: 3,
+            source: function(query, process) {
+                SCWeb.core.Server.findIdentifiersSubStr(query, function(data) {
+                    keys = [];
+                    for (key in data) {
+                        var list = data[key];
+                        for (idx in list) {
+                            var value = list[idx]
+                            keys.push(value[1]);
+                            self.addrs[value[1]] = value[0];
+                        }
+                    }
+
+                    process(keys);
+                });
+            },
+            updater: function(item) {
+                var addr = self.addrs[item];
+                if (addr)
+                    SCWeb.core.Main.doDefaultCommand([addr]);
+            }
+        });
+
+        dfd.resolve();
+
+        return dfd.promise();
+    },
+    
+};
