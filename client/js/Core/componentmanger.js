f530aa7d915a45486473e28ce367048f63b49772
Bobkov Andrey <bobr8949@yandex.ru>
2016-10-21 19:14:09 +0300

    fix subscribeWindow, delete SCn-editor js

--- client/js/Core/componentmanger.js
+++ client/js/Core/componentmanger.js
@@ -131,10 +131,10 @@ SCWeb.core.ComponentManager = {
             var component = comp_def.factory(sandbox);
             if(component.editor){
                 if(component.editor.keyboardCallbacks){
-                    SCWeb.ui.KeyboardHandler.subscribeWindow(options.container_selector, component.editor.keyboardCallbacks);
+                    SCWeb.ui.KeyboardHandler.subscribeWindow(options.window_id, component.editor.keyboardCallbacks);
                 }
                 if(component.editor.openComponentCallbacks) {
-                    SCWeb.ui.OpenComponentHandler.subscribeComponent(options.container_selector, component.editor.openComponentCallbacks);
+                    SCWeb.ui.OpenComponentHandler.subscribeComponent(options.window_id, component.editor.openComponentCallbacks);
                 }
             }
             if (component) {
