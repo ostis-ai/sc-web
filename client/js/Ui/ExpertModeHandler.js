0832214556ea966f9b43c3a9cfdc36949f2f514c
KovalM <michail.kovalev7@gmail.com>
2018-12-05 08:46:32 +0300

    fix(expert-mode): Return hiding mode switching panel when expert mode off

--- client/js/Ui/ExpertModeHandler.js
+++ client/js/Ui/ExpertModeHandler.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
     c = document.querySelector('#mode-switching-checkbox');
     c.checked = SCWeb.core.ExpertModeEnabled;
     c.onclick = function () {
+        document.getElementsByClassName("mode-switching-panel")[0].style.display = c.checked ? "" : "none";
         SCWeb.core.ExpertModeEnabled = c.checked;
         SCWeb.core.EventManager.emit("expert_mode_changed");
     };
