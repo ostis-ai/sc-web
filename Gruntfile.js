e6cee42d544456c8591d656ba01df8211c01abbf
Mikhail Sadouski <Mikhail_Sadouski@epam.com>
2018-12-06 02:14:27 +0300

    Correction of whitespaces for Grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,7 +44,7 @@ module.exports = function(grunt) {
                       webCoreCompPath + 'Ui/windowmanager.js',
                       webCoreCompPath + 'Ui/OpenComponentHandler.js',
                       webCoreCompPath + 'Ui/userpanel.js',
-					  webCoreCompPath + 'Ui/expertmodepanel.js',
+                      webCoreCompPath + 'Ui/expertmodepanel.js',
                       webCoreCompPath + 'Ui/ExpertModeHandler.js',
                 ],
                 dest: clientJsDirPath + 'sc-web-core.js',
