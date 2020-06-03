8f688b5a3330a59578f5906e6650461e3e1d0084
MikhailSadovsky <mishachess@mail.ru>
2020-06-03 21:29:52 +0300

    fix(search): search by system identifier implemented

--- client/js/Ui/searchpanel.js
+++ client/js/Ui/searchpanel.js
@@ -1,25 +1,29 @@
 const searchByKeyWord = (event, item) => {
-    if (item && item.addr) {
-        if (item.name) {
-            if (/^[A-Za-z0-9_]*$/.test(item.name)) {
-                SCWeb.core.Server.resolveScAddr([item.name], function (addrs) {
-                    if ($.isEmptyObject(addrs)) {
-                        console.log('Given identifier exists in redis db but not in rocksdb');
-                    } else {
-                        SCWeb.core.Main.doDefaultCommand([addrs[item.name]]);
-                    }
-                });
-            } else {
-                console.log('Please note that search can be done by english identifiers only in this version');
-            }
+    if (item.addr) {
+        SCWeb.core.Main.doDefaultCommand([item.addr]);
+    } else if (item.name) {
+        if (/^[A-Za-z0-9_ ]*$/.test(item.name)) {
+            searchByIdentifier(item.name);
         } else {
-            SCWeb.core.Main.doDefaultCommand([item.addr]);
+            console.log('Please note that search can be done by english identifiers only in this version');
         }
+    } else {
+        searchByIdentifier(item);
     }
     event.stopPropagation();
     $('.typeahead').val('');
     $('.tt-dropdown-menu').hide();
 };
+
+const searchByIdentifier = (identifier) => {
+    SCWeb.core.Server.resolveScAddr([identifier], function (addrs) {
+        if ($.isEmptyObject(addrs)) {
+            console.log('Given identifier exists in redis db but not in rocksdb');
+        } else {
+            SCWeb.core.Main.doDefaultCommand([addrs[identifier]]);
+        }
+    });
+}
 
 SCWeb.ui.SearchPanel = {
     init: function () {
@@ -37,8 +41,9 @@ SCWeb.ui.SearchPanel = {
             {
                 name: 'idtf',
                 source: function (query, cb) {
-                    $('#search-input').addClass('search-processing');
-                    SCWeb.core.Server.findIdentifiersSubStr(query, function (data) {
+                    //$('#search-input').addClass('search-processing');
+                    // TODO implement for rocksdb
+/*                    SCWeb.core.Server.findIdentifiersSubStr(query, function (data) {
                         keys = [];
 
                         var addValues = function (key) {
@@ -57,7 +62,7 @@ SCWeb.ui.SearchPanel = {
 
                         cb(keys);
                         $('#search-input').removeClass('search-processing');
-                    });
+                    });*/
                 },
                 displayKey: 'name',
                 templates: {
@@ -82,7 +87,7 @@ SCWeb.ui.SearchPanel = {
             searchByKeyWord(event, item);
         }).keypress(function (event) {
             if (event.which === 13) {
-                searchByKeyWord(event, keys[0]);
+                searchByKeyWord(event, $('#search-input').val());
             }
         });
 
