f4e777825789dd380d545a92077b289db8824441
Valentin Vityaz <valik.vityaz@gmail.com>
2020-09-18 10:03:45 +0300

    build(sc-web): fix package install

--- scripts/install_nodejs_dependence.sh
+++ scripts/install_nodejs_dependence.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-sudo apt-get install nodejs
-sudo apt-get install npm
+nodeVersion=$(apt-cache policy nodejs | grep -oP "(?<=Candidate:\s)(.+)(?=)")
+if [[ "${nodeVersion}" =~ ^8\.10\.0~dfsg-2ubuntu0\.[3-9] ]] # 8.10.0~dfsg-2ubuntu0.3+ conflicts with libcurl4-openssl-dev
+then
+	sudo apt-get install -y nodejs=8.10.0~dfsg-2ubuntu0.2 nodejs-dev=8.10.0~dfsg-2ubuntu0.2
+else
+	sudo apt-get install -y nodejs
+fi
+
+sudo apt-get install -y npm
 sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
