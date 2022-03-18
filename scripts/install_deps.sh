c809e22b38cfb2370f6aa2c76a4e36ab6ff303d8
VasKho <vasya.khoroshavin@gmail.com>
2022-03-18 09:12:40 +0300

    Fix python version

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,4 +1,4 @@
-sudo apt install --no-install-recommends -y python3.9 python3-pip nodejs npm
+sudo apt install --no-install-recommends -y python3 python3-pip nodejs npm
 
 pip3 install tornado sqlalchemy python-rocksdb progress numpy configparser py-sc-client
 
