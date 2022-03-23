a602b8c55ad84eae1ec9bc16cdc1b75b3990048a
VasKho <vasya.khoroshavin@gmail.com>
2022-03-23 20:08:54 +0300

    Rename install_deps.sh

--- scripts/install_deps.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install --no-install-recommends -y python3 python3-pip nodejs npm
-
-pip3 install tornado sqlalchemy python-rocksdb progress numpy configparser py-sc-client
-
-npm install grunt-cli
