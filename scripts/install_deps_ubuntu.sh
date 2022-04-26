9129484d618fbd2c83cabd2d7f9b0130cb9bc7ad
VasKho <73379561+VasKho@users.noreply.github.com>
2022-04-26 17:23:36 +0300

    Update ubuntu deps installation

--- scripts/install_deps_ubuntu.sh
+++ scripts/install_deps_ubuntu.sh
@@ -1,2 +1,3 @@
-sudo apt-get install -y python3-pip libsnappy-dev libbz2-dev liblz4-dev
-sudo yes | python3 -m pip install tornado==6.1.0 sqlalchemy python-rocksdb==0.7.0 progress==1.5 numpy configparser py-sc-client
+sudo apt install --no-install-recommends -y python3 python3-pip nodejs npm
+
+npm install grunt-cli
