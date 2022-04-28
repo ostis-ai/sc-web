a01a307ff1135e039e36f19aed4119ddc7d74711
VasKho <vasya.khoroshavin@gmail.com>
2022-04-28 17:03:29 +0300

    Fix run_scweb script

--- scripts/run_scweb.sh
+++ scripts/run_scweb.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 cd ../server/
-python3 app.py
+CONFIG_FILE_PATH="../../sc-machine/config/sc-machine.ini"
+if [ $# -ne 0 ]; then
+    CONFIG_FILE_PATH=$1
+fi
+echo $CONFIG_FILE_PATH
+# python3 app.py --cfg=$CONFIG_FILE_PATH
 cd -
