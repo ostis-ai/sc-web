ca9674d5a98a4a7ba5257fb3615f7f7766c5fe01
VasKho <vasya.khoroshavin@gmail.com>
2022-04-28 17:07:57 +0300

    Improved running script

--- scripts/run_scweb.sh
+++ scripts/run_scweb.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 cd ../server/
-CONFIG_FILE_PATH="../../sc-machine/config/sc-machine.ini"
 if [ $# -ne 0 ]; then
-    CONFIG_FILE_PATH=$1
+    python3 app.py --cfg=$1
+else
+    python3 app.py
 fi
-echo $CONFIG_FILE_PATH
-# python3 app.py --cfg=$CONFIG_FILE_PATH
 cd -
