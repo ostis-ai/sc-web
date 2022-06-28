245b107854c9165ee119765251d189014f7b7b1a
Dzmitry Brutski <dzmitry.brutski@gmail.com>
2022-06-28 15:33:15 +0300

    install client

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,5 +1,5 @@
 sudo apt install -y python python3-pip nodejs npm
 
-pip3 install tornado sqlalchemy python-rocksdb progress numpy configparser
+pip3 install tornado sqlalchemy python-rocksdb progress numpy configparser py-sc-client
 
 npm install grunt-cli
--- scripts/install_deps_ubuntu.sh
+++ scripts/install_deps_ubuntu.sh
@@ -1,2 +1,2 @@
 sudo apt-get install -y python3-pip libsnappy-dev libbz2-dev liblz4-dev
-sudo yes | python3 -m pip install tornado==6.1.0 sqlalchemy python-rocksdb==0.7.0 progress==1.5 numpy configparser
+sudo yes | python3 -m pip install tornado==6.1.0 sqlalchemy python-rocksdb==0.7.0 progress==1.5 numpy configparser py-sc-client
