18f677f888d0b71516affa8a9652e9c5ed3fecad
Dzmitry Brutski <dzmitry.brutski@gmail.com>
2022-06-28 15:33:15 +0300

    feat(py-server): install py-sc-client

--- scripts/install_deps_ubuntu.sh
+++ scripts/install_deps_ubuntu.sh
@@ -1,1 +1,2 @@
-sudo apt install --no-install-recommends -y python3 python3-pip nodejs npm
+sudo apt-get install -y python3-pip libsnappy-dev libbz2-dev liblz4-dev
+sudo yes | python3 -m pip install tornado==6.1.0 sqlalchemy python-rocksdb==0.7.0 progress==1.5 numpy configparser py-sc-client
