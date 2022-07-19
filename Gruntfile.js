4460695b11e3f1c87d94611ccf49a00c7266ef20
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-07-19 23:36:49 +0300

    feat: update grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,9 @@ module.exports = function(grunt) {
     grunt.initConfig({
         concat: {
             webcore: {
-                src: ["node_modules/ts-sc-client/build/sc.js",
+                src: [
+                      "node_modules/ts-sc-client/build/sc.js",
+                      webCoreCompPath + 'Utils/sc_types.js',
                       webCoreCompPath + 'Utils/sc_keynodes.js',
                       webCoreCompPath + 'Utils/utils.js',
                       webCoreCompPath + 'Utils/sc_helper.js',
