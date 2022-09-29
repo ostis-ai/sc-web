117d1b2a90a21fbaefb43667acfe3f2fe90cec66
Vladimir Moshchuk <ardonplay@Vladimirs-MacBook-Air.local>
2022-09-29 22:50:32 +0300

    add script for macOS

---
+++ scripts/install_deps_macOS.sh
@@ -0,0 +1,10 @@
+brew install python3
+# download and install setuptools
+curl -O https://bootstrap.pypa.io/ez_setup.py
+python3 ez_setup.py
+# download and install pip
+curl -O https://bootstrap.pypa.io/get-pip.py
+
+python3 get-pip.py
+brew install  nodejs
+brew install npm
