7036b5410a18b374ccc2d9125a65f63d972a6e0f
FallenChromium <fallenchromium@gmail.com>
2022-10-21 12:56:27 +0300

    fix: use official nodejs repo

--- scripts/install_deps_ubuntu.sh
+++ scripts/install_deps_ubuntu.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo chmod a+r /usr/share/keyrings/nodesource.gpg
+sudo apt update
 sudo apt install --no-install-recommends -y python3 python3-pip nodejs
