e57516b3d350a87acd03073527652f3ac6d69307
Aleksandra Efimova <sashaefimova007@gmail.com>
2023-02-09 00:52:13 +0300

    Don't show start page in case of scg view

--- server/handlers/main.py
+++ server/handlers/main.py
@@ -12,4 +12,10 @@ class MainHandler(base.BaseHandler):
     def get(self):
         first_time = self.get_cookie("first_time", "!")
         self.set_cookie("first_time", "0")
-        self.render("base.html", has_entered=False, user=self.current_user, first_time=first_time, public_url=self.public_url)
+        page = "base.html"
+        sys_id = self.get_argument('sys_id', None)
+        scg_view = self.get_argument('scg_structure_view_only', None)
+        # in case of scg view request we don't need to show start page
+        if sys_id is not None and scg_view is not None:
+            page = "mainWithoutContent.html"
+        self.render(page, has_entered=False, user=self.current_user, first_time=first_time, public_url=self.public_url)
