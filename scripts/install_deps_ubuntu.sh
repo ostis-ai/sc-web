5f7cd3f48da66e8da2da48dc285ed6ef7156fdfd
SHtress <stas.butrin1331@gmail.com>
2023-04-06 22:35:04 +0300

    fix: change apt to apt-get for use in scripts

--- scripts/install_deps_ubuntu.sh
+++ scripts/install_deps_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-sudo apt install curl
+sudo apt-get install curl
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo chmod a+r /usr/share/keyrings/nodesource.gpg
-sudo apt update
-sudo apt install --no-install-recommends -y python3 python3-pip nodejs
-sudo apt autoremove
+sudo apt-get update
+sudo apt-get install --no-install-recommends -y python3 python3-pip nodejs
+sudo apt-get autoremove
