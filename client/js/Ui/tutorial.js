17c8b3f729f51f8507cbbc73df7f01a7eabf2e18
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2023-05-03 11:09:29 +0300

    feat: preliminary tutorial element route

--- client/js/Ui/tutorial.js
+++ client/js/Ui/tutorial.js
@@ -1,9 +1,32 @@
 SCWeb.ui.Tutorial = {
+    inProgress: false,
+    tutorialStageSelectors: [
+        "#search-panel",
+        "#search-input",
+        "#tutorial-suggestion",
+        "#expert_mode_container",
+        "#window-type-select-button",
+        "#scg-mode-toggle-button",
+        "#create-node-button",
+        "#expert_mode_container"
+    ],
 
     init: function() {
         return new Promise(resolve => {
-            alert(3);
             resolve();
+        });
+    },
+
+    isInProgress: function() {
+        return this.inProgress;
+    },
+
+    begin: function() {
+        this.inProgress = true;
+        this.currentStage = -1;
+        this.commenceToNextStage();
+    },
+
         })
     }
 }
