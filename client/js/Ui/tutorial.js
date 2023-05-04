6b5530a2f11720adb44bb325b8f74e99bfd9990f
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2023-05-04 19:53:47 +0300

    feat: tutorial

--- client/js/Ui/tutorial.js
+++ client/js/Ui/tutorial.js
@@ -28,127 +28,118 @@ SCWeb.ui.Tutorial = {
                 elemID: "#search-panel",
                 desc: {
                     rus: {
-                        title: '',
-                        text: ''
+                        title: 'Поле поиска',
+                        text: 'Добро пожаловать в обучение! Здесь Вы узнаете об основном функционале системы. Это окно всегда можно скрыть нажатием комбинации клавиш "Shift + H". Также обучение всегда можно завершить досрочно нажатием кнопки "Abort tutorial" в правом нижнем углу этого окна. Ваше первое задание - поиск сущности в базе знаний по идентификатору. Нажмите на поле поиска.'
                     },
                     eng: {
                         title: '',
-                        text: ''
+                        text: 'a'
                     }
-                },
-                completed: false
+                }
             },
             {
                 elemID: "#search-input",
                 desc: {
                     rus: {
-                        title: '',
-                        text: ''
+                        title: 'Поиск по основному идентификатору',
+                        text: 'Это - поле поиска сущности по идентификатору. В него можно вводить как стандартные, так и системные идентификаторы. Давайте найдём сущность с основным идентификатором "транзитивное отношение".'
                     },
                     eng: {
-                        title: '',
-                        text: ''
+                        title: 'a',
+                        text: 'a'
                     }
-                },
-                completed: false
+                }
             },
             {
                 elemID: "#tutorial-suggestion",
                 desc: {
                     rus: {
-                        title: '',
-                        text: ''
+                        title: 'Выбор сущности из списка',
+                        text: 'Выберите сущность с идентификатором "транзитивное отношение" из выпадающего списка.'
                     },
                     eng: {
-                        title: '',
-                        text: ''
+                        title: 'a',
+                        text: 'a'
                     }
-                },
-                completed: false
+                }
             },
             {
                 elemID: "#expert_mode_container",
                 desc: {
                     rus: {
-                        title: '',
-                        text: ''
+                        title: 'Экспертный режим',
+                        text: 'Это - окно просмотра информации о сущности. Здесь отображаются все сущности, с которыми связана данная сущность, а также отношения, их связывающие. Нажатие левой кнопкой мыши по идентификатору любой сущности откроет подобное окно для выбранной сущности. Однако, по умолчанию, в этом окне опускается информация, которая, вероятнее всего, не будет полезна большинству пользователей. Для отображения всей имеющейся информации о сущности, а также для использования расширенного функционала по просмотру и редактированию базы знаний, используется экспертный режим. Перейдите в экспертный режим.'
                     },
                     eng: {
-                        title: '',
-                        text: ''
+                        title: 'a',
+                        text: 'a'
                     }
-                },
-                completed: false
+                }
             },
             {
                 elemID: "#window-type-select-button",
                 desc: {
                     rus: {
-                        title: '',
-                        text: ''
+                        title: 'Элемент управления режимом отображения',
+                        text: 'Вы вошли в экспертный режим. В экспертном режиме отображается полная информация о желаемой сущности, а также открыт доступ к дополнительным инструментам просмотра и редактирования базы знаний. В частности, среди доступных инструментов в правой верхней панели можно видеть элемент управления режимом отображения информации. На данный момент таких режимов доступно два - SCn-код (то, что Вы видите сейчас) и SCg-код (графическое представление). Давайте перейдём в режим графического представления информации о сущности. Нажмите на выделенную кнопку.'
                     },
                     eng: {
-                        title: '',
-                        text: ''
+                        title: 'a',
+                        text: 'a'
                     }
-                },
-                completed: false
+                }
             },
             {
                 elemID: "#scg-mode-toggle-button",
                 desc: {
                     rus: {
-                        title: '',
-                        text: ''
+                        title: 'Переключение в графический режим отображения',
+                        text: 'Нажмите на кнопку с текстом SCg-код.'
                     },
                     eng: {
-                        title: '',
-                        text: ''
+                        title: 'a',
+                        text: 'a'
                     }
-                },
-                completed: false
+                }
             },
             {
                 elemID: "#window-container",
                 desc: {
                     rus: {
-                        title: '',
-                        text: ''
+                        title: 'Создание узла',
+                        text: 'Вы перешли в графический режим представления информации. Здесь отображены те же самые сущности и отношения, что и ранее, в виде графа. Помимо просмотра, графический режим можно использовать и для редактирования базы знаний. Редактирование базы знаний подразумевает под собой создание, редактирование или удаление узлов или дуг. Давайте создадим новый узел. Создание нового узла доступно в режиме выделения (кнопка с изображением курсора мыши в боковой панели инструментов) путём двойного нажатия левой кнопки мыши по пустому месту на полотне. Создайте новый узел.'
                     },
                     eng: {
-                        title: '',
-                        text: ''
+                        title: 'a',
+                        text: 'a'
                     }
-                },
-                completed: false
+                }
             },
             {
                 elemID: "#scg-tool-edge",
                 desc: {
                     rus: {
-                        title: '',
-                        text: ''
+                        title: 'Режим создания дуг',
+                        text: 'Вы создали узел в базе знаний. Однако изолированный узел, не связанный ни с какими другими узлами никакими отношениями, несёт мало пользы для БЗ. Давайте создадим дугу, связывающую этот узел и некий другой существующий узел. Перейдите в режим создания дуг.'
                     },
                     eng: {
-                        title: '',
-                        text: ''
+                        title: 'a',
+                        text: 'a'
                     }
-                },
-                completed: false
+                }
             },
             {
                 elemID: "#window-container",
                 desc: {
                     rus: {
-                        title: '',
-                        text: ''
+                        title: 'Создание дуги',
+                        text: 'Вы перешли в режим создания дуги. Создать дугу можно путём нажатия левой кнопки мыши сначала на узле-источнике, а после - на узле назначения. Создайте дугу, соединяющую два существующих узла.'
                     },
                     eng: {
-                        title: '',
-                        text: ''
+                        title: 'a',
+                        text: 'a'
                     }
-                },
-                completed: false
+                }
             }
         ];
     },
@@ -180,6 +171,7 @@ SCWeb.ui.Tutorial = {
             this.end();
             return;
         }
+        this.updateInfo();
         this.addStageCompletionListener(this.currentStage);
     },
 
@@ -205,6 +197,39 @@ SCWeb.ui.Tutorial = {
 
     toggleInfoVisibility: function() {
         $('#tutorial-info').toggleClass('visible');
+    },
+
+    updateInfo: function() {
+        const tutorialListContainer = $('#tutorial-info-left');
+        tutorialListContainer.empty();
+        this.tutorialStages.forEach((stage, index) => tutorialListContainer.append(this.getInfoListpoint(index)));
+        this.updateTaskDescription();
+    },
+
+    getInfoListpoint: function(stageIndex) {
+        const isCurrentStage = stageIndex === this.currentStage;
+        const imagePath = `../static/components/images/${stageIndex > this.currentStage - 1 ? 'red-cross' : 'green-tick'}.png`;
+        const stageObj = this.tutorialStages[stageIndex];
+        const title = stageObj.desc.rus.title;
+
+        const image = document.createElement('img');
+        image.setAttribute('src', imagePath);
+
+        const titleContainer = document.createElement(isCurrentStage ? 'b' : 'span');
+        titleContainer.append(title);
+
+        const listpoint = document.createElement('div');
+        listpoint.setAttribute('class', 'tutorial-listpoint');
+        listpoint.appendChild(image);
+        listpoint.appendChild(titleContainer);
+
+        return listpoint;
+    },
+
+    updateTaskDescription: function() {
+        $('#tutorial-point-description')
+            .empty()
+            .append(this.tutorialStages[this.currentStage].desc.rus.text);
     },
 
     hideInfo: function() {
