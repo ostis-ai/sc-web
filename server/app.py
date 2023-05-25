ca3930dee7ef7b01ad9df86836068d04bde17629
Chaika <s.v.chaikovsky@gmail.com>
2023-05-25 13:39:54 +0300

    fix: refactoring

--- server/app.py
+++ server/app.py
@@ -55,8 +55,7 @@ class NoCacheStaticHandler(tornado.web.StaticFileHandler, ABC):
     """
 
     def set_extra_headers(self, path):
-        self.set_header('Cache-Control',
-                        'no-store, no-cache, must-revalidate, max-age=0')
+        self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
 
 
 def main():
@@ -67,43 +66,31 @@ def main():
     tornado.options.define("templates_path", default=join(dirname(abspath(__file__)), "../client/templates"),
                            help="path to template files directory",
                            type=str)
-    tornado.options.define("event_wait_timeout", default=10,
-                           help="time to wait commands processing", type=int)
-    tornado.options.define("answer_wait_timeout", default=2,
-                           help="time to wait answer getting", type=int)
+    tornado.options.define("event_wait_timeout", default=10, help="time to wait commands processing", type=int)
+    tornado.options.define("answer_wait_timeout", default=2, help="time to wait answer getting", type=int)
     tornado.options.define("idtf_search_limit", default=100,
                            help="number of maximum results for searching by identifier", type=int)
-    tornado.options.define("host", default="localhost",
-                           help="host name", type=str)
+    tornado.options.define("host", default="localhost", help="host name", type=str)
     tornado.options.define("port", default=8000, help="host port", type=int)
     tornado.options.define(
         "server_host", default="localhost", help="host name", type=str)
-    tornado.options.define("server_port", default=8090,
-                           help="host port", type=int)
+    tornado.options.define("server_port", default=8090, help="host port", type=int)
     tornado.options.define(
         "public_url", default="ws://localhost:8090/ws_json", help="public server url", type=str)
-    tornado.options.define("auth_redirect_port",
-                           default=80, help="host port", type=int)
+    tornado.options.define("auth_redirect_port", default=80, help="host port", type=int)
 
-    tornado.options.define("google_client_id", default="",
-                           help="client id for google auth", type=str)
-    tornado.options.define("google_client_secret", default="",
-                           help="client secret for google auth", type=str)
+    tornado.options.define("google_client_id", default="", help="client id for google auth", type=str)
+    tornado.options.define("google_client_secret", default="", help="client secret for google auth", type=str)
 
-    tornado.options.define("apiai_subscription_key", default="",
-                           help="subscription key for api.ai", type=str)
-    tornado.options.define("apiai_client_access_token", default="",
-                           help="client access token for api.ai", type=str)
+    tornado.options.define("apiai_subscription_key", default="", help="subscription key for api.ai", type=str)
+    tornado.options.define("apiai_client_access_token", default="", help="client access token for api.ai", type=str)
 
-    tornado.options.define("user_key_expire_time", default=600,
-                           help="user key expire time in seconds", type=int)
+    tornado.options.define("user_key_expire_time", default=600, help="user key expire time in seconds", type=int)
     tornado.options.define("super_emails", default="", help="email of site super administrator (maximum rights)",
                            type=list)
-    tornado.options.define("db_path", default="data.db",
-                           help="path to database file", type=str)
+    tornado.options.define("db_path", default="data.db", help="path to database file", type=str)
 
-    tornado.options.define(
-        "cfg", default="", help="path to configuration file", type=str)
+    tornado.options.define("cfg", default="", help="path to configuration file", type=str)
 
     # parse config
     tornado.options.parse_command_line()
@@ -140,8 +127,7 @@ def main():
     rules = [
         (r"/", MainHandler),
 
-        (r"/static/(.*)", NoCacheStaticHandler,
-         {"path": tornado.options.options.static_path}),
+        (r"/static/(.*)", NoCacheStaticHandler, {"path": tornado.options.options.static_path}),
 
         # api
         (r"/api/context/", api.ContextMenu),
@@ -183,8 +169,7 @@ def main():
     application.listen(tornado.options.options.port)
     tornado.ioloop.PeriodicCallback(try_exit, 1000).start()
     app_instance = tornado.ioloop.IOLoop.instance()
-    signal.signal(signal.SIGINT, lambda sig,
-                  frame: app_instance.add_callback_from_signal(on_shutdown))
+    signal.signal(signal.SIGINT, lambda sig, frame: app_instance.add_callback_from_signal(on_shutdown))
     app_instance.start()
 
     logger.disabled = False
