e8dadbe4555f37b508daf873393ad4c54dd20713
NikitaZotov <nikita.zotov.belarus@gmail.com>
2023-06-20 18:32:22 +0300

    fix(components-sandbox): check edge on addArcEventRequest and removeArcEventRequest

--- client/js/Core/componentsandbox.js
+++ client/js/Core/componentsandbox.js
@@ -67,16 +67,16 @@ SCWeb.core.ComponentSandbox = function (options) {
         let addArcEventRequest = new sc.ScEventParams(
             new sc.ScAddr(this.addr),
             sc.ScEventType.AddOutgoingEdge,
-            (elAddr, edge, otherAddr) => {
-                if (self.eventStructUpdate) {
+            async (elAddr, edge, otherAddr) => {
+                if (self.eventStructUpdate && ((await scClient.checkElements([edge]))[0].value & sc_type_arc_pos_const_perm)) {
                     self.eventStructUpdate(true, elAddr.value, edge.value);
                 }
             });
         let removeArcEventRequest = new sc.ScEventParams(
             new sc.ScAddr(this.addr),
             sc.ScEventType.RemoveOutgoingEdge,
-            (elAddr, edge, otherAddr) => {
-                if (self.eventStructUpdate) {
+            async (elAddr, edge, otherAddr) => {
+                if (self.eventStructUpdate && ((await scClient.checkElements([edge]))[0].value & sc_type_arc_pos_const_perm)) {
                     self.eventStructUpdate(false, elAddr.value, edge.value);
                 }
             });
