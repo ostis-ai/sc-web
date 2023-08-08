0c7f50a59e1508084fbcc7d15fefee5a895bfff4
NikitaZotov <nikita.zotov.belarus@gmail.com>
2023-08-08 18:54:19 +0300

    fix: remove reinstall node for macOS

--- scripts/install_deps_macOS.sh
+++ scripts/install_deps_macOS.sh
@@ -6,12 +6,7 @@ then
   source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"/set_vars.sh
 fi
 
-brew install curl
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo chmod a+r /usr/share/keyrings/nodesource.gpg
-brew update
-brew install python3
-brew install node
+brew install python3 node
 
 pip3 install -r "${SC_WEB_PATH}/requirements.txt"
 
