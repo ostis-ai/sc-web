9838e59b68dfd113838a10b7fb0c32a6ed81abb3
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-05-01 11:41:29 +0300

    scripts: use venv for python dependencies

--- scripts/install_deps_python.sh
+++ scripts/install_deps_python.sh
@@ -6,6 +6,7 @@ then
   source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"/set_vars.sh
 fi
 
+python3 -m venv "${SC_WEB_PATH}/.venv"
+source "${SC_WEB_PATH}/.venv/bin/activate"
 pip3 install wheel setuptools
 pip3 install -r "${SC_WEB_PATH}/requirements.txt"
-
