1d5bcb237e988c86c6749d28ba391b7c1025333e
kuanysh-zhaksylyk <kuan.zhk@gmail.com>
2024-06-13 17:05:59 +0300

    feat: update build sc web script to use specific version of node

--- scripts/build_sc_web.sh
+++ scripts/build_sc_web.sh
@@ -11,7 +11,13 @@ fi
 
 stage "Build sc-web"
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 cd "${SC_WEB_PATH}"
+NODE_MAJOR=16
+
+nvm use ${NODE_MAJOR}
 npm run build
 
 stage "SC-web built successfully"
