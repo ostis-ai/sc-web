c39d89e4ab811f75acac231079cdef35e8833e8b
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-12-02 14:08:22 +0300

    scripts: use nvm for macOS

--- scripts/install_deps_macOS.sh
+++ scripts/install_deps_macOS.sh
@@ -6,7 +6,21 @@ then
   source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"/set_vars.sh
 fi
 
-brew install python3 node
+brew install python3
+
+export NVM_DIR="$HOME/.nvm"
+if [ ! -d "$NVM_DIR" ]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+NODE_MAJOR=16
+
+nvm install ${NODE_MAJOR} 
+nvm use ${NODE_MAJOR} 
 
 "${SC_WEB_PATH}/scripts/install_deps_npm.sh"
 "${SC_WEB_PATH}/scripts/install_deps_python.sh"
